91108d10fdb2b1ea9b1015bd5e1ec266c6e75aa3
hundredvisionsguy <cwinikka@gmail.com>
2017-03-17 17:22:41 -0700

    reducing length of code lines

--- cardTrumpChecker/TestCardTrumpChecker.py
+++ cardTrumpChecker/TestCardTrumpChecker.py
@@ -8,32 +8,37 @@ class KnownValues(unittest.TestCase):
 
     def test_cardTrumpCheckerForLowNumHighColor(self):
         # Capture the results of the function
-        result = cardTrumpChecker.cardTrumpChecker(('card1', 5, 'yellow'), ('card2', 7, 'green') )
+        result = cardTrumpChecker.cardTrumpChecker(('card1', 5, 'yellow'),
+                                                   ('card2', 7, 'green') )
         # Check for expected output
         self.assertEqual("card2", result)
 
     def test_cardTrumpCheckerForSameNumHighColor(self):
         # Capture the results of the function
-        result = cardTrumpChecker.cardTrumpChecker(('card1', 5, 'red'), ('card2', 5, 'yellow') )
+        result = cardTrumpChecker.cardTrumpChecker(('card1', 5, 'red'),
+                                                   ('card2', 5, 'yellow') )
         # Check for expected output
         self.assertEqual("card1", result)
 
     def test_cardTrumpCheckerForSameNumLowColor(self):
         # Capture the results of the function
-        result = cardTrumpChecker.cardTrumpChecker(('card1', 10, 'green'), ('card2', 10, 'yellow') )
+        result = cardTrumpChecker.cardTrumpChecker(('card1', 10, 'green'),
+                                                   ('card2', 10, 'yellow') )
         # Check for expected output
         self.assertEqual("card2")
 
     def test_cardTrumpCheckerForHighNumLowColor(self):
         # Capture the results of the function
-        result = cardTrumpChecker.cardTrumpChecker(('card1', 2, 'green'), ('card2', 1, 'red'))
+        result = cardTrumpChecker.cardTrumpChecker(('card1', 2, 'green'),
+                                                   ('card2', 1, 'red'))
         # Check for expected output
         self.assertEqual("card1", result)
 
 
     def test_cardTrumpCheckerForSameCardSameNum(self):
         # Capture the results of the function
-        result = cardTrumpChecker.cardTrumpChecker(('card1', 10, 'red'), ('card2', 10, 'red'))
+        result = cardTrumpChecker.cardTrumpChecker(('card1', 10, 'red'),
+                                                   ('card2', 10, 'red'))
         # Check for expected output
         self.assertEqual("card1", result)
 
